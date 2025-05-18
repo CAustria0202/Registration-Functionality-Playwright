6c69e80f4804e7278625d659c65a31a48da3fb89
Tian01301 <c.austria@toyota-mobilitysolutions.ph>
2025-05-18 16:31:21 +0800

    sample sharding

--- playwright.config.js
+++ playwright.config.js
@@ -22,12 +22,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'],
-  ["line"],
-  ["allure-playwright"]
-  ],
+  reporter: process.env.CI? 'blob' : 'html',
   timeout: 30000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
